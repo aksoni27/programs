c05142bb3437d85be649c3451ebac7e547241c1b
aksoni27 <42676217+aksoni27@users.noreply.github.com>
2019-04-09 00:57:37 +0530

    Update do.c

--- do.c
+++ do.c
@@ -1,42 +1,40 @@
-#include<stdio.h>
 #include<pthread.h>
-#include<sys/wait.h>
-#include<sys/types.h>
+#include<stdio.h>
 #include<unistd.h>
-void *fun1(void *a);
-void *fun2(void *a);
- int *q;
-void *fun1(void *a)
-{ int *d=(int *)a;
-  pthread_t th2;
-  int n=*d;
-  int first,second,sum=0,i=1;
-  int arr[n];
-  first=-1;
-  second=1;
-  while(i<=n)
+#include<sys/types.h>
+#include<sys/wait.h>
+void *func1(void *x);
+void *func2(void *x);
+int *array2;
+void *func1(void *x)
+{ pthread_t th2; 
+  int *b=(int *)x;
+  int n=*b;
+  int first=-1,second=1,sum=0;
+  int array[n];
+  for(int i=0;i<n;i++)
    {  sum=first+second;
       first=second;
       second=sum;
-      arr[i-1]=sum;
-      i++;
+      array[i]=sum;
    }
-   q=arr;
-   pthread_create(&th2,NULL,fun2,&n);
+   array2=array;
+   pthread_create(&th2,NULL,func2,&n);
    pthread_join(th2,NULL);
 }
-void *fun2(void *a)
-{  int *k=(int *)a;
-   int n=*k;;
-   for(int i=0;i<n;i++)
-     printf(" %d ",*(q+i));
+void *func2(void *x)
+{  int *b=(int *)x;
+   int n=*b;
+   printf("fibonacci series upto %d no. :\t ",n);
+   for(int i=0;i<n;i++)	
+     printf(" %d ",*(array2+i));
 }
 void main()
 {
-int a;
+int n;
 pthread_t th1;
-printf("Enter value of a\n");
-scanf("%d",&a);
-pthread_create(&th1,NULL,fun1,&a);
+printf("Enter value of n : ");
+scanf("%d",&n);
+pthread_create(&th1,NULL,func1,&n);
 pthread_join(th1,NULL);
 }
